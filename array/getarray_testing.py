3072f30ad59f63477790722dc166b17ccc21b96d
lenovo-pc_chati757 <po.chatipakorn_st@tni.ac.th>
2017-03-02 10:02:52 +0700

    array example

---
+++ array/getarray_testing.py
@@ -0,0 +1,7 @@
+#basic get content array position
+test_array =[['Hello', 'World1'], ['How', 'are', 'you?'], ['Bye', 'World2']]
+print("all array")
+print(test_array)
+print("array parser")
+print(test_array[2][1][0]) # output is [w] form World2
+print(test_array[0][1]) #world1
