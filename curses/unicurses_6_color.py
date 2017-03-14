e09990b8257dac3330030a248a98dfeb1cc8fe0b
lenovo-pc_chati757 <po.chatipakorn_st@tni.ac.th>
2017-03-14 17:12:24 +0700

    update 1712140317

--- curses/unicurses_6_color.py
+++ curses/unicurses_6_color.py
@@ -5,13 +5,21 @@ def main():
     stdscr = initscr() #start up screen and initalize ncurses
 
     start_color()
-    
+    use_default_colors()
     #use_default_colors() #using default color terminal background 
     init_pair(1,COLOR_RED,COLOR_WHITE) #init_pair(fg,bg) bg=-1 mean..tranparent
 
-    addstr("hello",color_pair(1)+A_REVERSE)
-    use_default_colors()
-    addstr("hello 2")
+    addstr("hello 1",color_pair(1)+A_REVERSE)
+
+    init_pair(2,14,-1) #init_pair(fg,bg) bg=-1 mean..tranparent
+    
+    addstr("hello 2",color_pair(2))
+
+    init_pair(3,11,-1) #init_pair(fg,bg) bg=-1 mean..tranparent
+    
+    addstr("hello 3",color_pair(3))
+
+    addstr("hello 4")
     getch()
     endwin() #for stop ncurses
 
