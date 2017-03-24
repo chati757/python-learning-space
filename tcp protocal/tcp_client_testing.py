d443c723acc2dc0f2076c8956f0f833cebdbbc14
lenovo-pc_chati757 <po.chatipakorn_st@tni.ac.th>
2017-03-24 16:57:33 +0700

    1657240317

--- tcp protocal/tcp_client_testing.py
+++ tcp protocal/tcp_client_testing.py
@@ -3,19 +3,26 @@ import socket
 def Main():
     host = '127.0.0.1'
     port = 3000
-
-    s = socket.socket()
-    s.connect((host,port))
-    message = "empty"
-    #message = raw_input("->")
-    #while message !=q:
-    while message !='server -s':
-        #s.send(message)
-        data = s.recv(3072)
-        print 'response form server: '+data
-        message = raw_input("->")
-        s.send(message)
-    s.close()
+    
+    try:
+        s = socket.socket()
+        s.connect((host,port))
+        message = "empty"
+        #message = raw_input("->")
+        #while message !=q:
+        while message !='server -s':
+            #s.send(message)
+            data = s.recv(3072)
+            print 'response form server: '+data
+            message = raw_input("->")
+            s.send(message)
+        s.close()
+    except IOError as e:
+        print(e.errno)
+        if(e.errno==10061):
+            print("cannot connect , trying..")
+        else:
+            print(e)
     
 if __name__ == '__main__':
     Main()
