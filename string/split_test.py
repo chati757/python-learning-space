d2da58436267ac1a903dc5495175ac91ad12e7e6
bch_chati757 <chati757@users.noreply.github.com>
2017-06-09 16:28:09 +0700

    update 9/6/17

--- string/split_test.py
+++ string/split_test.py
@@ -2,6 +2,7 @@ str = "Line1-abcdef Line2-abc Line4-abcd"
 print(len(str))
 print str.split()#['Line1-abcdef','Line2-abc','Line4-abcd']
 print str.split()[0]#Line1-abcdef
+print "form ls /etc:"
 print 'ls /etc'.split()
 
 str2 = "abc"*5
