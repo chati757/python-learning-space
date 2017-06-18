c0d93b0d514d3b00c7a0c0768d2b51adb098c8a2
bch_chati757 <chati757@users.noreply.github.com>
2017-06-18 18:05:51 +0700

    update 180660

--- urllib/useragent_testing.py
+++ urllib/useragent_testing.py
@@ -1,44 +1,35 @@
-'''
+# use for python3 only
 import urllib.request
 #checking : httpbin.org  
+
+proxy = urllib.request.ProxyHandler({'http':'http://127.0.0.1:8080'})
+opener = urllib.request.build_opener(proxy)
+urllib.request.install_opener(opener)
 
 req = urllib.request.Request(
     "http://httpbin.org/user-agent",
     data=None,
     headers={
-        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/35.0.1916.47 Safari/537.36'
+        'User-Agent': 'Mozilla/5.0 test123'
     }
 )
 
 f = urllib.request.urlopen(req)
 print(f.read().decode('utf-8'))
+
+'''
+# use for python2 only
+import urllib2
+url = "http://httpbin.org/user-agent"
+headers = {} #create object
+headers['User-Agent'] = "1234"
+
+proxy = urllib2.ProxyHandler({'http':'http://127.0.0.1:8080'})
+opener = urllib2.build_opener(proxy)
+urllib2.install_opener(opener)
+
+req = urllib2.Request(url,headers=headers)
+print urllib2.urlopen(req).read()
 '''
 
 
-#ref :https://pythonprogramming.net/urllib-tutorial-python-3/
-import urllib.request
-url = 'http://httpbin.org/user-agent'
-
-# now, with the below headers, we defined ourselves as a simpleton who is
-# still using internet explorer.
-headers = {}
-headers['User-Agent'] = "Mozilla/5.0"
-req = urllib.request.Request(url, headers = headers)
-resp = urllib.request.urlopen(req)
-respData = resp.read()
-print(respData)
-
-'''
-import urllib.request
-import sys
-req = urllib.request.Request(
-    "http://httpbin.org/user-agent"
-)
-
-f = urllib.request.urlopen(req)
-print(f.read().decode('utf-8'))
-f.close()
-
-print("sys.version[:3]")
-print(sys.version[:3])
-'''
