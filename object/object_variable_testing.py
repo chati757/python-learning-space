b63170a9a5f98641a09b058457b257cb1af2d65e
bch_chati757 <chati757@users.noreply.github.com>
2017-06-22 16:04:19 +0700

    update2206171604

---
+++ object/object_variable_testing.py
@@ -0,0 +1,29 @@
+
+test = {"t1":"test1","t2":"test2"}
+
+
+keys = test.keys()
+values = test.values()
+ 
+print(keys) #dict_keys(['t1','t2'])
+print(values) #dict_values(['test1','test2'])
+
+print("list key")
+print(list(keys)) #['t1','t2']
+print(list(keys)[0]) #t1
+print("list value")
+print(list(values)) #['test1','test2']
+print(list(values)[0]) #test1
+
+
+print(test.get('t1')) #test1
+
+'''
+>>> a = dict(one=1, two=2, three=3)
+>>> b = {'one': 1, 'two': 2, 'three': 3}
+>>> c = dict(zip(['one', 'two', 'three'], [1, 2, 3]))
+>>> d = dict([('two', 2), ('one', 1), ('three', 3)])
+>>> e = dict({'three': 3, 'one': 1, 'two': 2})
+>>> a == b == c == d == e
+True
+'''
