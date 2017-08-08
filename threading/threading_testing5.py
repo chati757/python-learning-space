d1f15d07f8769c0369fbb22a9770a6afb9d076ac
bch_chati757 <chati757@users.noreply.github.com>
2017-08-09 00:53:31 +0700

    98170053

--- threading/threading_testing5.py
+++ threading/threading_testing5.py
@@ -10,9 +10,15 @@ class FuncThread(threading.Thread):
         self._target(*self._args)
  
 # Example usage
-def someOtherFunc(data, key):
+def someOtherFunc(data,key,text):
     print "someOtherFunc was called : data=%s; key=%s" % (str(data), str(key))
+    print(text)
  
-t1 = FuncThread(someOtherFunc, [1,2], 6)
+t1 = FuncThread(someOtherFunc,[1,2], 6,"test")
 t1.start()
 t1.join()
+
+'''
+someOtherFunc was called : data=[1, 2]; key=6
+test
+'''
