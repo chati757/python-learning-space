356843efa67ea0e0ec59aa1973160699297f35b6
bch_chati757 <chati757@users.noreply.github.com>
2017-08-10 15:55:22 +0700

    1008171555

---
+++ threading/threading_8_returnvalue.py
@@ -0,0 +1,23 @@
+from threading import Thread
+
+def foo(bar):
+    print 'hello {0}'.format(bar)
+    return "foo"
+
+class ThreadWithReturnValue(Thread):
+    def __init__(self, group=None, target=None, name=None,
+                 args=(), kwargs={}, Verbose=None):
+        Thread.__init__(self, group, target, name, args, kwargs, Verbose)
+        self._return = None
+    def run(self):
+        if self._Thread__target is not None:
+            self._return = self._Thread__target(*self._Thread__args,
+                                                **self._Thread__kwargs)
+    def join(self):
+        Thread.join(self)
+        return self._return
+
+twrv = ThreadWithReturnValue(target=foo, args=('world!',))
+
+twrv.start()
+print twrv.join()   # prints foo
