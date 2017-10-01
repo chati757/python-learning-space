fc2e4fe1f8f4672eba4dc65e1619aa08201b651b
KuuGen Fox <chati757@users.noreply.github.com>
2017-10-01 22:09:54 +0700

    Update tcp_1_server_testing.py

--- tcp protocal/tcp_1_server_testing.py
+++ tcp protocal/tcp_1_server_testing.py
@@ -2,28 +2,30 @@ import socket
 import time
 
 def Main():
-    host = '127.0.0.1'
+    host = '192.168.1.35'
     port = 3000
 
     s = socket.socket()
     s.bind((host,port))
 
     s.listen(1)
-    print "server is running..."
-    print "waiting for client..."
-    c, addr = s.accept()
-    print "Connection from: "+str(addr)
-
-    c.send("server connected..")
-
-    while True:
-        data = c.recv(3072)
-        if data=="server -s":
-            break
-        print "send back form server to user: "+data
-        data = str(data).upper()
-        print "sending: "+data
-        c.send(data)
+    for i in range(2):
+        print("server is running...")
+        print("waiting for client...")
+        print(s)
+        print("server busy")
+        time.sleep(50) 
+        # ในขณะที่มัน busy โดย sleep มันจะ listen แค่ 1 client เท่านั้น หาก cline 2 หรือมากกว่านั้นเข้ามา
+        # client มันจะ error รหัส 10061 ทันที่ 
+        c,addr = s.accept()
+        print("Connection from: " +str(addr))
+    
+        data=c.recv(1024)
+        print(c)
+        print(str(data.decode("utf-8")))
+        re_msg="receive"
+        c.send(re_msg.encode("utf-8"))
+        print("delay")
     c.close()
 
 if __name__=='__main__':
