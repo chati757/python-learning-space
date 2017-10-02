6376596c34c6f7376cc205e7acc2358fbea155fa
bch_chati757 <chati757@users.noreply.github.com>
2017-10-02 14:19:50 +0700

    1419021017

--- threading/threading_testing3.py
+++ threading/threading_testing3.py
@@ -1,29 +1,35 @@
 import threading
 import queue
+import time
 
 cmdqueue = queue.Queue() # create queue object
 #--------------------------------worker structure------------------------------------------
-def workerThread(queobject):
+def workerThread(queobject,thread_id):
      while True:
-          vars = queobject.get()
-          print(vars)
-          queobject.task_done()
+        print("[",thread_id,"]worker wait queue")     
+        vars = queobject.get()
+        print(vars)
+        queobject.task_done()
 #--------------------------------create worker 4 worker------------------------------------
+thread_id=0
 for i in range(4):
-     worker = threading.Thread(target=workerThread, args=(cmdqueue,))
-     worker.setDaemon(True)#protect for zombie threads
-     '''
-     main thread is not daemon and All thread when we create in main 
-     the daemon thread default is set(False) when we build main thread.
-     if daemon thread is created in thread it's must to be set(True)
-     
-     '''
-     worker.start()
+    thread_id+=1
+    worker = threading.Thread(target=workerThread, args=(cmdqueue,thread_id,))
+    worker.setDaemon(True)#protect for zombie threads
+    '''
+    main thread is not daemon and All thread when we create in main 
+    the daemon thread default is set(False) when we build main thread.
+    if daemon thread is created in thread it's must to be set(True)
+
+    '''
+    worker.start()
 #--------------------------------put queue-------------------------------------------------
-for i in range(5):
-     data = {
-          'id': i,
-     }
-     cmdqueue.put(data)
+for i in range(10):
+    time.sleep(2)
+    data = {
+          'queue_id': i,
+    }
+    cmdqueue.put(data)
 cmdqueue.join()
+print("test")
 #------------------------------------------------------------------------------------------
