93ffc88674a02381229a8d7737d601bf73a48961
bch_chati757 <chati757@users.noreply.github.com>
2017-10-02 15:03:49 +0700

    1503021017

--- threading/threading_testing3.py
+++ threading/threading_testing3.py
@@ -6,12 +6,17 @@ cmdqueue = queue.Queue() # create queue object
 #--------------------------------worker structure------------------------------------------
 def workerThread(queobject,thread_id):
      while True:
-        print("[",thread_id,"]worker wait queue")     
+        print("[",thread_id,"]worker wait queue")    
         vars = queobject.get()
-        print(vars)
-        queobject.task_done()
+        if(vars==None):
+            print("[",thread_id,"]break")   
+            break  
+        print("[",thread_id,"]",vars)
+        queobject.task_done() # for thread tell queue is this thread is done
+        print("[",thread_id,"]task_done")
 #--------------------------------create worker 4 worker------------------------------------
 thread_id=0
+threads=[]
 for i in range(4):
     thread_id+=1
     worker = threading.Thread(target=workerThread, args=(cmdqueue,thread_id,))
@@ -23,13 +28,21 @@ for i in range(4):
 
     '''
     worker.start()
+    threads.append(worker)
 #--------------------------------put queue-------------------------------------------------
 for i in range(10):
-    time.sleep(2)
-    data = {
-          'queue_id': i,
-    }
+    #time.sleep(2)
+    data = i
     cmdqueue.put(data)
-cmdqueue.join()
-print("test")
+    
+cmdqueue.join() # block until all tasks are done
+
+print("stop worker")
+# stop workers
+for i in range(4):
+    cmdqueue.put(None)
+
+for t in threads:
+    t.join()
+print("end of main thread")
 #------------------------------------------------------------------------------------------
