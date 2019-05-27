522da4c2e2c262a161c35e35cc6ae45035a49449
bch_chati757 <chati757@users.noreply.github.com>
2019-05-27 23:35:23 +0700

    update tabulate topic

---
+++ tabulate/example.py
@@ -0,0 +1,31 @@
+#pandas dataframe as a nice text-based table with tabulate
+
+from tabulate import tabulate
+import pandas as pd
+
+df = pd.DataFrame({'col_two' : [0.0001, 1e-005 , 1e-006, 1e-007],'column_3' : ['ABCD', 'ABCD', 'long string', 'ABCD']})
+
+print(tabulate(df,headers='keys', tablefmt='psql',colalign=("center","center","left")))
+
+"""
++----+-----------+-------------+
+|    |  col_two  |  column_3   |
+|----+-----------+-------------|
+| 0  |  0.0001   |    ABCD     |
+| 1  |   1e-05   |    ABCD     |
+| 2  |   1e-06   | long string |
+| 3  |   1e-07   |    ABCD     |
++----+-----------+-------------+
+"""
+
+print(tabulate(df,showindex="never", headers='keys', tablefmt='psql',colalign=("center","left")))
+"""
++-----------+-------------+
+|  col_two  | column_3    |
+|-----------+-------------|
+|  0.0001   | ABCD        |
+|   1e-05   | ABCD        |
+|   1e-06   | long string |
+|   1e-07   | ABCD        |
++-----------+-------------+
+"""
