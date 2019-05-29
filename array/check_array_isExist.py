d199cbdc1b99fea31b2258bd7461b1dd14202ff5
bch_chati757 <chati757@users.noreply.github.com>
2019-05-29 15:19:23 +0700

    update check array is exist

---
+++ array/check_array_isExist.py
@@ -0,0 +1,15 @@
+test = ['a','c','d']
+
+data1 = 'a'
+data2 = 'j'
+
+if(data1 not in test):
+     print("not in")
+else:
+    print(data1 , "in array")
+
+
+if(data2 not in test):
+     print("not in")
+else:
+    print(data2 , "in array")
