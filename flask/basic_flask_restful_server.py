35198d92c71ffb75f7ba2d115d598388e41d84bf
bch_chati757 <chati757@users.noreply.github.com>
2019-05-29 23:15:12 +0700

    update comment of explain

--- flask/basic_flask_restful_server.py
+++ flask/basic_flask_restful_server.py
@@ -5,10 +5,29 @@ app = Flask(__name__)
 api = Api(app)
 
 class Student(Resource):
-    def get(self,name):
-        return {'student':name}
+    def get(self,name=None):
+        buff = None
+        if(name!=None):
+            buff = {'student':name}
+        else:
+            buff = {'hello':'world'}
+        return buff
 
-api.add_resource(Student,'/student/<string:name>')
+#http://localhost:5000/
+"""
+#http://localhost:5000/
+{
+    "hello":"world"
+}
+"""
+#http://localhost:5000/student/bob
+"""
+{
+"student": "bob"
+}
+"""
+
+api.add_resource(Student,'/','/student/<string:name>')
 
 if(__name__=='__main__'):
     app.run(port=5000,debug=True)
