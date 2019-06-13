8dc1801afcbc961d79a612c91ba84d43171eaf5b
bch_chati757 <chati757@users.noreply.github.com>
2019-06-13 17:18:23 +0700

    update stdout logging

--- logging/advance_stdout_logging.py
+++ logging/advance_stdout_logging.py
@@ -4,13 +4,9 @@ import logging
 Logging Level 
 
 [10]DEBUG: Detailed infomation , typically of interest only when diagnosing problems.
-
 [20]INFO: Confirmation that things are working as expected.
-
 [30]WARNING: An indication that something unexpected hanppened , or indicative of some problem in the near future Ex. disk space low , the software is still working as expected.
-
 [40]ERROR:Due to a more serious problem, the software has not been able to proform some function.
-
 [50]CRITICAL:A serious error , indicating that the program itself may be unable to continue running.
 """
 
@@ -24,18 +20,27 @@ def multiply(x,y):
     return x * y
 
 def devide(x,y):
-    return x / y
+    try:
+        result = x / y
+    except ZeroDivisionError:
+        #main_logger.error('ZeroDivisonError') # it'will be show message only in advance_testng.log
+        #if you need to throw error and message 
+        main_logger.exception('ZeroDivisonError')
+    else:
+        return result
 
 if __name__ == "__main__":
     #default level logging is warnning , it's will be aleart in level warnning and higher
 
-    #change default logging level with debug, change format
-    #ref for change logging format : https://docs.python.org/3/library/logging.html#logrecord-attributes
-    logging.basicConfig(level=logging.DEBUG,format='%(asctime)s:%(levelname)s:%(name)s:%(message)s')
+    #create main logging instant
+    main_logger = logging.getLogger("main")
+    main_logger.setLevel(logging.INFO)
 
-    num_1 = 10
-    num_2 = 5
+    main_logger_formatter = logging.Formatter('%(asctime)s:%(levelname)s:%(name)s:%(message)s')
+    main_logger_handler = logging.StreamHandler(stream=None)
 
-    add_result = add(num_1,num_2)
-    logging.debug('add : {} + {} = {}'.format(num_1,num_2,add_result)) #DEBUG:root:add : 10 + 5 = 15
-    logging.warning('add : {} + {} = {}'.format(num_1,num_2,add_result)) #WARNING:root:add : 10 + 5 = 15
+    main_logger_handler.setFormatter(main_logger_formatter)
+
+    main_logger.addHandler(main_logger_handler )
+
+    main_logger.info("test")
