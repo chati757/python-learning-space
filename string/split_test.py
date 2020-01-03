19ea997099a31445a455deefb8a35828943ee51c
bch_chati757 <chati757@users.noreply.github.com>
2020-01-03 21:51:27 +0700

    update split to except multiple space

--- string/split_test.py
+++ string/split_test.py
@@ -1,14 +1,14 @@
-str = "Line1-abcdef Line2-abc Line4-abcd"
-print(len(str))
-print str.split()#['Line1-abcdef','Line2-abc','Line4-abcd']
-print str.split()[0]#Line1-abcdef
+str1 = "Line1-abcdef Line2-abc Line4-abcd"
+print(len(str1))
+print str1.split()#['Line1-abcdef','Line2-abc','Line4-abcd']
+print str1.split()[0]#Line1-abcdef
 print "ls/etc:"
 print 'ls/etc'.split("/")
 
 str2 = "abc"*5
 print(str2)
 
-print(" ".join(str.split())) #is opposite of split command
+print(" ".join(str1.split())) #is opposite of split command
 
 print("testlen")
 test=""
@@ -17,3 +17,7 @@ print(len(test))
 str3 = "abc"
 print(str3[:-1])
 print(len(str3))
+
+#using except multiple space
+str4 = "test1    test2"
+print(str4.split()) # [test1,test2]
