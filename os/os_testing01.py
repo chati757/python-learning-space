c1e8daf65f5a20ec02a9b00a9abff94252d6a35b
bch_chati757 <chati757@users.noreply.github.com>
2020-01-03 22:52:16 +0700

    update check file exist

--- os/os_testing01.py
+++ os/os_testing01.py
@@ -1,4 +1,5 @@
 import os
+#print(os.path.isfile(fname)) # return False or True (for check file is exist)
 print(os.path.isdir("./home/el")) # return False or True
 print(os.listdir(".")) # list file in current directory
 os.makedirs("./test") # create folder name is "test"
