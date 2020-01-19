ba6ba25c80bec7ca3c062637e7e8fcf4877d385e
bch_chati757 <chati757@users.noreply.github.com>
2020-01-19 17:16:39 +0700

    add more example about if and if-else

--- list comprehensions/list_comprehensions1.py
+++ list comprehensions/list_comprehensions1.py
@@ -24,6 +24,12 @@ sum([i+1 for i in range(period)])/period
 
 #condition example
 arr = [1,2,3,4,5,6]
+
+#if state
 [i for i in arr if i > 2]
 #[3, 4, 5, 6]
+
+#if else state
+[i if i > 2 else i+1 for i in arr]
+#[2, 3, 4, 5, 6]
 '''
