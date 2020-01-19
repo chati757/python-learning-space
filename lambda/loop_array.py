f6d07dc0e732fdc6a7f952a47bdd7f36157c6cb6
bch_chati757 <chati757@users.noreply.github.com>
2020-01-19 17:18:24 +0700

    add more example (group continue of array) and (filter continue in minimum range of array)

--- lambda/loop_array.py
+++ lambda/loop_array.py
@@ -24,7 +24,12 @@ print(f2) #[6]
 f = lambda v : True if (v>0) else False
 print(f(1))
 print(f(-1))
+#--------------------------------------------loop in loop---------------------------------------------
+pos = [[0, 1, 2], [7, 8], [10], [14, 15]]
+print([j for i in pos for j in i]) #[0, 1, 2, 7, 8, 10, 14, 15]
+#-----------------------------------------------------------------------------------------------------
 
+#--------------------------------------------rate analyze---------------------------------------------
 x_rate = {
         'very bad':-0.4,
         'bad':-0.1334,
@@ -52,3 +57,84 @@ def test(x_rate,x):
     return res
 print(sys.getsizeof(test(x_rate,x)))
 print(test(x_rate,x))
+#-----------------------------------------------------------------------------------------------------
+#---------------------------------group continue of array---------------------------------------------
+'''
+input = [9,4,1,4,6,1]
+#filter < 5
+af = [4,1,4,1] 
+#convert to index
+[c for c,i in enumerate(af)] #[1,2,3,5] --> arrx:[1,2,3,5] --> result:[[1,2,3],[5]] --> last_result:[[4,1,4],[1]]
+'''
+#[0,1,2,7,8,9,10,13,14,15] --> [[0,1,2],[7,8,9,10],[13,14,15]]
+arrx = [0,1,2,7,8,9,10,13,14,15]
+
+arry = arrx.copy()[1:] + [None] #[1,2,7,8,9,10,13,14,15,None]
+
+arrxy = list(map(lambda arrx,arry: [arrx,arry],arrx,arry)) #[[0, 1], [1, 2], [2, 7], [7, 8], [8, 9], [9, 10], [10, 13], [13, 14], [14, 15], [15, None]]
+
+arrxy_filter = list(filter(lambda x: x[0]+1 != x[1], arrxy)) #[[2,7],[10,13],[15,None]]
+
+#arrxy_reduce_dimention = [i[0] for i in arrxy_filter] + [i[1] for i in arrxy_filter] #[2, 10, 15, 7, 13, None]
+
+arrxy_reduce_dimention = [i[0]+i[1] for i in [arrxy_filter[:-1]]][0] #[2, 7, 10, 13]
+arrxy_reduce_dimention = [arrx[0]] + arrxy_reduce_dimention + arrx[:-1] #[0, 2, 7, 10, 13, 15]
+
+#marge range from pair of array
+even_arr = [i for c,i in enumerate(arrxy_reduce_dimention) if c%2==0]#[0, 7, 13]
+
+odd_arr = [i for c,i in enumerate(arrxy_reduce_dimention) if c%2!=0]#[2, 10, 15]
+
+re = list(map(lambda x,y:[x,y],even_arr,odd_arr))#[[0, 2], [7, 10], [13, 15]]
+
+result = [arrx[arrx.index(i[0]):(arrx.index(i[1])+1)] for i in re] #[[0, 1, 2], [7, 8, 9, 10], [13, 14, 15]]
+#-----------------------------------------------------------------------------------------------------
+#-------------------------group continue of array (var.2)---------------------------------------------
+#[0,1,2,7,8,9,10,13,14,15] --> [[0,1,2],[7,8,9,10],[13,14,15]]
+arrx = [0,1,2,7,8,10,14,15]
+
+arry = arrx.copy()[1:] + [None] #[1,2,7,8,9,10,13,14,15,None]
+
+end = [i for c,i in enumerate(arrx) if(arrx[c]+1)!=arry[c]] #[2, 10, 15]
+
+start = [0] + [arrx[c+1] for c,i in enumerate(arrx) if (arrx[c]+1)!=arry[c] and arry[c]!=None] #[0, 7, 13]
+
+re = list(map(lambda x,y:[x,y],start,end))#[[0, 2], [7, 10], [13, 15]]
+
+result = [arrx[arrx.index(i[0]):(arrx.index(i[1])+1)] for i in re] #[[0, 1, 2], [7, 8, 9, 10], [13, 14, 15]]
+
+print([j for i in pos for j in [result]]) #[[0, 1, 2], [7, 8, 9, 10], [13, 14, 15]]
+'''
+#syntax compression
+arrx = [0,1,2,7,8,10,14,15]
+arry = arrx.copy()[1:] + [None]
+[arrx[arrx.index(i[0]):(arrx.index(i[1])+1)] for i in list(map(lambda x,y:[x,y],([0] + [arrx[c+1] for c,i in enumerate(arrx) if (arrx[c]+1)!=arry[c] and arry[c]!=None]),([i for c,i in enumerate(arrx) if(arrx[c]+1)!=arry[c]])))]
+'''
+#-----------------------------------------------------------------------------------------------------
+#-----------------filter continue in minimum range of array (var.2)-----------------------------------
+'''
+input = [4,3,1,6,7,8,4,3,9,1,7,8,9,3,1]
+#example : filter < 5
+output = [[4, 3, 1], [4, 3], [1], [3, 1]]
+'''
+inputx = [4,3,1,6,7,8,4,3,9,1,7,8,9,3,1]
+arrx = [c for c,i in enumerate(inputx) if i<5] # [0, 1, 2, 6, 7, 9, 13, 14]
+
+arry = arrx.copy()[1:] + [None] # [1, 2, 6, 7, 9, 13, 14, None]
+
+end = [i for c,i in enumerate(arrx) if(arrx[c]+1)!=arry[c]] #[2, 7, 9, 14]
+
+start = [0] + [arrx[c+1] for c,i in enumerate(arrx) if (arrx[c]+1)!=arry[c] and arry[c]!=None] #[0, 6, 9, 13]
+
+re = list(map(lambda x,y:[x,y],start,end))#[[0, 2], [6, 7], [9, 9], [13, 14]]
+
+result = [inputx[(i[0]):((i[1])+1)] for i in re] #[[4, 3, 1], [4, 3], [1], [3, 1]]
+
+'''
+#syntax compression
+inputx = [4,3,1,6,7,8,4,3,9,1,7,8,9,3,1]
+arrx = [c for c,i in enumerate(inputx) if i<5] # [0, 1, 2, 6, 7, 9, 13, 14]
+arry = arrx.copy()[1:] + [None]
+[inputx[i[0]:(i[1]+1)] for i in list(map(lambda x,y:[x,y],([0] + [arrx[c+1] for c,i in enumerate(arrx) if (arrx[c]+1)!=arry[c] and arry[c]!=None]),([i for c,i in enumerate(arrx) if(arrx[c]+1)!=arry[c]])))]
+'''
+#-----------------------------------------------------------------------------------------------------
