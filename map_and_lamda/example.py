a7db33406bd64614ebabb1626bf67efc9bc4004f
bch_chati757 <chati757@users.noreply.github.com>
2020-01-19 17:14:25 +0700

    add more example (map multiple function and return in arry)

--- map_and_lamda/example.py
+++ map_and_lamda/example.py
@@ -4,3 +4,22 @@ test_arr2 = [6,8,1]
 result = map(lambda x,y:x+y,test_arr1,test_arr2)
 print(result)
 print(list(result))
+
+'''
+def multiply(x):
+    return (x*x)
+def add(x):
+    return (x+x)
+
+funcs = [multiply, add]
+for i in range(5):
+    value = list(map(lambda x: x(i), funcs))
+    print(value)
+
+# Output:
+# [0, 0]
+# [1, 2]
+# [4, 4]
+# [9, 6]
+# [16, 8]
+'''
