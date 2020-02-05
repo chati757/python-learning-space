e6edb55a5c0b6ca26253546e4a9336b0f2cd33af
bch_chati757 <chati757@users.noreply.github.com>
2020-02-05 16:07:55 +0700

    update os example

--- os/os_testing01.py
+++ os/os_testing01.py
@@ -1,7 +1,7 @@
 import os
 #print(os.path.isfile(fname)) # return False or True (for check file is exist)
 print(os.path.isdir("./home/el")) # return False or True
-print(os.listdir(".")) # list file in current directory
+print(os.listdir("./empty")) # list file in current directory
 #os.makedirs("./test") # create folder name is "test"
 
 #os.rmdir('./data_txt/test_dir') #for delete file
