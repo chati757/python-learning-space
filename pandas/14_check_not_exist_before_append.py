98db8d05fbdc5f0b258447fec39cd73583080622
bch_chati757 <chati757@users.noreply.github.com>
2020-08-15 12:42:35 +0700

    update ex

--- pandas/14_check_not_exist_before_append.py
+++ pandas/14_check_not_exist_before_append.py
@@ -13,6 +13,9 @@ df = pd.DataFrame({'a':[1,2,2,1,1],'b':[9,2,2,9,9]})
 
 #if it's exist it's pass and not append
 try: 
-    df.loc[len(check_df)] = {'a':1,'b':9} if(df.append({'a':1,'b':9},ignore_index=True).duplicated().any()==False) else []
-except:
+    df.loc[len(df)] = {'a':1,'b':9} if(df.append({'a':1,'b':9},ignore_index=True).duplicated().any()==False) else []
+except ValueError:
+    print('inexcept')
     pass
+
+print(df)
