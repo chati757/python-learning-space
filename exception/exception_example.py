38d31bde754fd99b2ac8c2cb13ae6f4862cbf6ca
bch_chati757 <chati757@users.noreply.github.com>
2021-06-08 23:59:40 +0700

    resolved syntax

--- exception/exception_example.py
+++ exception/exception_example.py
@@ -4,7 +4,7 @@ try:
    try:
       fh.write("This is my test file for exception handling!!")
    finally:
-      print "Going to close the file"
+      print("Going to close the file")
       fh.close()
 except IOError:
-   print "Error: can\'t find file or read data"
+   print("Error: can\'t find file or read data")
