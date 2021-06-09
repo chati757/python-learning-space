c682569dfc40c9e27f1e43211b6a1bd24f5ee391
bch_chati757 <chati757@users.noreply.github.com>
2021-06-09 18:00:16 +0700

    update example

--- exception/exception_example.py
+++ exception/exception_example.py
@@ -2,9 +2,15 @@
 try:
    fh = open("testfile", "w")
    try:
-      fh.write("This is my test file for exception handling!!")
+      raise IOError
    finally:
       print("Going to close the file")
       fh.close()
+
 except IOError:
    print("Error: can\'t find file or read data")
+
+#not goto exception below
+except Exception as e:
+   print('inexcept')
+   print(e)
