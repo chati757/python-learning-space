6ced451c9dcb8c8115068d076575d34677d60ab5
bch_chati757 <chati757@users.noreply.github.com>
2022-05-22 22:19:45 +0700

    test source , play sound and config host

--- eel/main.py
+++ eel/main.py
@@ -10,13 +10,10 @@ import eel
 df = pd.DataFrame({'stock':['ptt','bcp'],'sector':['energy','energy'],'grade':['a','c'],'hlfc':['4','2'],'rsi10':['33.234','30.955'],'rsi_wtb':['6:w.4-5:t.3:tp.2-4(lower)','6:w.4-5:t.3:tp.2-4(lower)'],'inv_harami_pattern':['TRUE','FALSE'],'double_pattern':['FALSE','TRUE']})
 
 eel.init("web")
-eel.start("main.html",block=False)
+eel.start("main.html",host="192.168.2.36",block=False)
 #calculate rsi10 set100 and get rsi10_val and rsi10_level
 #eel.update_set100_rsi10(rsi10_val,rsi10_level)
 eel.fetch_table_data(df.to_json(orient="values"))
-eel.update_highlight_hlfc()
-eel.update_highlight_inv_harami_pattern()
-eel.update_highlight_double_pattern()
 while True:
     print('active')
     eel.sleep(10)
